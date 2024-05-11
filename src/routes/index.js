a38b780d24c42007e32684f5aba39f69d7fa87f0
Karen <karen.denich@gmail.com>
2024-05-11 16:07:41 -0300

    tabelas criadas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,10 @@
 const { Router } = require('express')
 
-//users.routes.js
 const usersRoutes = require('./users.routes')
-const filmRoutes = require('./film.routes')
+const notesRoutes = require('./notes.routes')
 
 const routes = Router()
-
-routes.use('/user', usersRoutes)
-routes.use('/film', filmRoutes)
-
-//film.routes.js
-
-//tag.routes.js
+routes.use('/users', usersRoutes)
+routes.use('/notes', notesRoutes)
 
 module.exports = routes
